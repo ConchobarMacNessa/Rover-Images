c7c09ad5c95216caa5249c3efbccf2b8339191a7
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-04-15 11:56:28 +0100

    initial file set up set up hapi server, basic routes, gitignore config.env

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const hapi = require('hapi');
+const server = new hapi.Server();
+const inert = require('inert');
+const vision = require('vision');
+const routes = require('./routes');
+
+server.connection({
+  host: 'localhost',
+  port: 4000,
+});
+
+server.register([inert, vision], (err) => {
+  if (err) throw err;
+  server.route(routes);
+});
+
+module.exports = server;
